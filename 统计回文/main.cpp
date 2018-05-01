378043395587c400796d24d2547302919f188666
yang_qiang <yang_qiang@aliyun.com>
2018-05-01 18:32:36 +0800

    添加统计回文

---
+++ 统计回文/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_palindrome(const string str){
+    int i = 0;
+    int len = str.length();
+    for( i; i < len/2; i++){
+        if( str[i] != str[len - 1 - i] ){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, const char *argv[]){
+    string s1 = string(argv[1]);
+    string s2 = string(argv[2]);
+    
+    int len = s1.length();
+    int count = 0;
+    string tmp;
+#if 1
+    for( int i = 0; i <= len; i++ ){
+        tmp = s1;
+        tmp.insert(i, s2);
+        if( is_palindrome(tmp) ){
+            count++;
+        }
+    }
+#endif
+    cout << count << endl;
+    return 0;
+}
